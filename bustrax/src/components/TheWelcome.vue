843158358577b7a6e13da0cae61ab8c075a0ce0a
Humberto Santos <1201hs@gmail.com>
2024-01-31 08:14:41 -0600

    first commit

---
+++ bustrax/src/components/TheWelcome.vue
@@ -0,0 +1,21 @@
+<script>
+import { createApp, ref } from 'vue'
+
+export default {
+  setup() {
+    const count = ref(0);
+
+    return {
+      count
+    };
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <button @click="count++">
+      Count is: {{ count }}
+    </button>
+  </div>
+</template>
